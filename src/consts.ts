fcd8281a487402345798d9a284ff0fa568475eb6
TheGarlicBear <266297431+TheGarlicBear@users.noreply.github.com>
2026-03-08 15:21:56 +0900

    chore: update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "aiwithyou.dev";
+export const SITE_DESCRIPTION = "Astro + Vercel로 운영하는 정적 기술 블로그";
